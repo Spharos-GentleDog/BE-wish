6583b2bc3c89781ad71bfdd97e748f7fedd66b2f
Hwan0518 <qip0518@naver.com>
2023-11-22 12:06:13 +0900

    feat: BoilerPlate코드 생성

---
+++ src/main/java/egenius/wish/global/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package egenius.wish.global.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Gentledog Wish API",
+                version = "v1",
+                description = "Wish-service API Docs for Gentledog"
+        )
+)
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        String[] paths = {"/api/v1/**"};
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
